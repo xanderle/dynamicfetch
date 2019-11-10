277cdc88202056b5f0f67f0626cebb3eb45f554a
Alexander Emery <x.marks.the.spot.xd@gmail.com>
2019-11-10 15:15:51 +1100

    Initial Commit

---
+++ dynamicFetch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dynamicFetch
+//
+//  Created by Alexander Emery on 10/11/19.
+//  Copyright © 2019 Alexander Emery. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
